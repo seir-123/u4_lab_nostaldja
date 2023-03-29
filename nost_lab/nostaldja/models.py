b4250427446326386bf33d54b2803748c032afe2
Benjamin <benjamin.lewin@yahoo.com>
2023-03-29 16:36:44 -0400

    finished

---
+++ nost_lab/nostaldja/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Decade(models.Model):
+    name = models.CharField(max_length=100)
+    start_year = models.CharField(max_length=100)
+    end_year = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Fad(models.Model):
+    name = models.CharField(max_length=100)
+    image_url = models.TextField()
+    description = models.CharField(max_length=200)
+    decade = models.ForeignKey(Decade, on_delete=models.CASCADE, related_name='fads')
+
+    def __str__(self):
+        return self.name
+    
