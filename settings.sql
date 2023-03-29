80450251931f0030d172dfef34ceeee27605d708
narniaeagle <ilgar2023@hotmail.com>
2023-03-29 16:22:11 -0400

    d

---
+++ settings.sql
@@ -0,0 +1,4 @@
+-- settings.sql
+CREATE DATABASE nostaldja;
+CREATE USER nostaldjauser WITH PASSWORD 'tunr';
+GRANT ALL PRIVILEGES ON DATABASE tunr TO nostaldjauser;
---
+++ tunr/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import  Decade
+from .models import Fad
+
+admin.site.register(Decade)
+admin.site.register(Fad)
+
+# Register your models here.
